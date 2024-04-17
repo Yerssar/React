03175e3fd6b0c92d536eabec981a17cc2e2be5cb
Erbol <yers2106gmail.com>
2024-04-17 17:12:23 +0600

    add components lesson02

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="">
+      <Button />
+      <Button />
+      <Button />
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
