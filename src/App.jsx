81ba92306f70183afa6c24f55264ed8f96f11702
Erbol <yers2106gmail.com>
2024-04-21 20:36:30 +0600

    add hw03

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "./App.css";
 // import Lesson02 from "./lessons/Lessons02/Lessons02";
 // import Homework02 from "./homeworks/";
 // import Lesson03 from "./lessons/Lesson03/Lesson03";
-import Homework03 from "./homeworks/Homework03/Homework03";
+// import Homework03 from "./homeworks/Homework03/Homework03";
+import Lesson04 from "./lessons/Lesson04/Lesson04";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <Lesson02 /> */}
       {/* <Homework02 /> */}
       {/* <Lesson03 /> */}
-      <Homework03 />
+      {/* <Homework03 /> */}
+      <Lesson04 />
     </div>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,25 @@
 import "./styles.css";
 
-function Button({ name, type }) {
+function Button({ name, type = "button", onButtonClick }) {
   return (
-    <button className="button-component" type={type}>
+    <button className="button-component" type={type} onClick={onButtonClick}>
       {name}
     </button>
   );
 }
 
 export default Button;
+
+
+
+// import "./styles.css";
+
+// function Button({ name, type }) {
+//   return (
+//     <button className="button-component" type={type}>
+//       {name}
+//     </button>
+//   );
+// }
+
+// export default Button;
